3ef9bf8e7c0f38369789255cf582d6b16aa41948
Bastian Allgeier <mail@bastianallgeier.com>
2020-07-13 12:40:15 +0200

    Fix indentation

--- src/Formats.js
+++ src/Formats.js
@@ -1,67 +1,67 @@
 export default {
-    bold: {
-        html(content) {
-            return `<strong>${content}</strong>`;
-        },
-        parser(node) {
-            return ["B", "STRONG"].includes(node.nodeName);
-        }
+  bold: {
+    html(content) {
+      return `<strong>${content}</strong>`;
     },
-    code: {
-        html(content) {
-            return `<code>${content}</code>`;
-        },
-        parser(node) {
-            return node.nodeName === "CODE";
-        }
+    parser(node) {
+      return ["B", "STRONG"].includes(node.nodeName);
+    }
+  },
+  code: {
+    html(content) {
+      return `<code>${content}</code>`;
     },
-    italic: {
-        html(content) {
-            return `<em>${content}</em>`;
-        },
-        parser(node) {
-            return ["I", "EM"].includes(node.nodeName);
-        }
+    parser(node) {
+      return node.nodeName === "CODE";
+    }
+  },
+  italic: {
+    html(content) {
+      return `<em>${content}</em>`;
     },
-    link: {
-        html(content, attr) {
-            return `<a href="${attr.href}">${content}</a>`;
-        },
-        parser(node) {
-            if (node.nodeName !== "A") {
-                return false;
-            }
+    parser(node) {
+      return ["I", "EM"].includes(node.nodeName);
+    }
+  },
+  link: {
+    html(content, attr) {
+      return `<a href="${attr.href}">${content}</a>`;
+    },
+    parser(node) {
+      if (node.nodeName !== "A") {
+        return false;
+      }
 
-            return {
-                href: node.getAttribute("href"),
-                rel: node.getAttribute("rel"),
-                target: node.getAttribute("target"),
-                title: node.getAttribute("title"),
-            };
-        }
+      return {
+        href: node.getAttribute("href"),
+        rel: node.getAttribute("rel"),
+        target: node.getAttribute("target"),
+        title: node.getAttribute("title"),
+      };
+    }
+  },
+  strikeThrough: {
+    html(content) {
+      return `<del>${content}</del>`;
     },
-    strikeThrough: {
-        html(content) {
-            return `<del>${content}</del>`;
-        },
-        parser(node) {
-            return node.nodeName === "DEL";
-        }
+    parser(node) {
+      return node.nodeName === "DEL";
+    }
+  },
+  subscript: {
+    html(content) {
+      return `<sub>${content}</sub>`;
     },
-    subscript: {
-        html(content) {
-            return `<sub>${content}</sub>`;
-        },
-        parser(node) {
-            return node.nodeName === "SUB";
-        }
+    parser(node) {
+      return node.nodeName === "SUB";
+    }
+  },
+  superscript: {
+    html(content) {
+      return `<sup>${content}</sup>`;
     },
-    superscript: {
-        html(content) {
-            return `<sup>${content}</sup>`;
-        },
-        parser(node) {
-            return node.nodeName === "SUP";
-        }
-    },
+    parser(node) {
+      return node.nodeName === "SUP";
+    }
+  },
 };
